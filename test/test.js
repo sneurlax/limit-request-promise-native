18b31d84344ba4abbf62d039ecf4574e9480e7b6
sneurlax <sneurlax@gmail.com>
2018-07-30 09:46:00 -0700

    linting

--- test/test.js
+++ test/test.js
@@ -1,22 +1,24 @@
-var assert = require("assert");
+var assert = require('assert');
 var task =require('promise-util-task');
 var LimitRequestPromise = require('..');
 
-describe("simple", function() {
-    it("test", function(done) {
-        this.timeout(5000);
-        var lr = new LimitRequestPromise(1, 1);
-        var urls = [
-            'http://www.yahoo.co.jp',
-            'http://www.yahoo.co.jp',
-            'http://www.yahoo.co.jp',
-            'http://www.yahoo.co.jp'
-        ]
-        var tasklist = urls.map(function(v){ return function(){ return lr.req(v) } })
-        task.all(tasklist).then(function(){
-            done()
-        }).catch(function(e){
-            done(e)
-        })
-    });
+describe('simple', function() {
+  it('test', function(done) {
+    this.timeout(5000);
+    var lr = new LimitRequestPromise(1, 1);
+    var urls = [
+      'http://www.yahoo.co.jp',
+      'http://www.yahoo.co.jp',
+      'http://www.yahoo.co.jp',
+      'http://www.yahoo.co.jp'
+    ]
+    var tasklist = urls.map(function(v) { return function() { return lr.req(v); } })
+    task.all(tasklist)
+    .then(function({
+      done()
+    })
+    .catch(function(e) {
+      done(e)
+    })
+  });
 });
